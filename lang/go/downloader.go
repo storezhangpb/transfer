09cc9b0717f243b5833fbf29d7192fa00a346318
storezhang <storezhang@gmail.com>
2022-03-08 15:05:51 +0800

    完善文件上传下载

--- lang/go/downloader.go
+++ lang/go/downloader.go
@@ -1,7 +1,5 @@
 package transfer
 
-// downloader 下载
 type downloader interface {
-	// Download 下载文件
 	Download(src string, dest string, base string) (err error)
 }
--- lang/go/uploader.go
+++ lang/go/uploader.go
@@ -1,7 +1,5 @@
 package transfer
 
-// uploader 上传
 type uploader interface {
-	// Upload 上传文件
 	Upload(dest string, src string, base string) (err error)
 }
