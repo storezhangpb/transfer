d56125284137e3dee7ec933af4219b741c003cf7
storezhang <storezhang@gmail.com>
2022-03-09 10:18:05 +0800

    完善文件上传下载

--- lang/go/type.pb.go
+++ lang/go/type.pb.go
@@ -1,12 +1,12 @@
 package transfer
 
 const (
-	Type_TYPE_UNKNOWN = 0
-	Type_HTTP         = 1
-	Type_FTP          = 1
-	Type_OSS          = 1
-	Type_COS          = 1
-	Type_LOCAL        = 1
+	Type_TYPE_UNKNOWN Type = 0
+	Type_HTTP         Type = 1
+	Type_FTP          Type = 1
+	Type_OSS          Type = 1
+	Type_COS          Type = 1
+	Type_LOCAL        Type = 1
 )
 
 type Type int
